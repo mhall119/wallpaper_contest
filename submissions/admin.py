f149a499098d1812f3bdf1bc95f1544d4445ab32
Michael Hall <mhall119@gmail.com>
2018-03-13 13:26:10 -0400

    Add edit link to results page, add view link to submission admin

--- submissions/admin.py
+++ submissions/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from submissions.models import Contest, Category, Submission, Vote
 # Register your models here.
 
@@ -6,8 +8,10 @@ admin.site.register(Contest)
 admin.site.register(Category)
 
 class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'contest')
-    list_filter = ('contest', 'author')
+    def view(self, photo):
+        return mark_safe('<a href="%s" target="_blank">[view]</a>' % photo.image_url)
+    list_display = ('title', 'view', 'author', 'contest', 'category')
+    list_filter = ('contest', 'category', 'author')
 admin.site.register(Submission, SubmissionAdmin)
 
 class VoteAdmin(admin.ModelAdmin):
